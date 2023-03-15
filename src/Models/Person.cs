2622348872ff23313c013e24f4b3a01ed674e052
prime167 <3722845+prime167@users.noreply.github.com>
2023-03-15 13:13:25 +0800

    Play with C# 12: Primary Constructors

---
+++ src/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace UsingDotNET.DirMover.Models;
+
+public class Person(string name, int age)
+{
+    public string Name { get; set; } = name;
+
+    public int Age { get; set; } = age;
+
+    public Person() : this("Jack", 16)
+    {
+    }
+}
